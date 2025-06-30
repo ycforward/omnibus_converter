1a392cabb03f59fe32331f8d243dbc5a26a963ce
Yerong Chen <ychen@Yerongs-Mac-Studio.local>
2025-06-30 22:13:21 +0800

    Lock app orientation to portrait mode only (prevents UI breakage in landscape)

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,13 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'screens/home_screen.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await SystemChrome.setPreferredOrientations([
+    DeviceOrientation.portraitUp,
+    DeviceOrientation.portraitDown,
+  ]);
   runApp(const ConverterApp());
 }
 
