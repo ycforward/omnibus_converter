a85b092731c8a1b6465b0fad4ddcc0ed67ca7042
Yerong Chen <ychen@Yerongs-Mac-Studio.local>
2025-07-06 22:02:43 +0800

    Add missing CurrencyService to fix compilation (non-UI service)

--- lib/services/currency_service.dart
+++ lib/services/currency_service.dart
@@ -1,1 +1,6 @@
- 
+class CurrencyService {
+  /// Get the default list of currencies for the app
+  static List<String> getDefaultCurrencies() {
+    return ['USD', 'CNY', 'EUR', 'GBP', 'JPY', 'CAD', 'AUD', 'CHF', 'INR', 'BRL'];
+  }
+} 
